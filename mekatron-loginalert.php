b7eb0f9d09ef124c2d5c36cba5a235a3e50e0961
MuhammadRezaHeidary <muhammadreza.heidary@gmail.com>
2023-05-23 22:16:33 +0330

    telegram bot

--- mekatron-loginalert.php
+++ mekatron-loginalert.php
@@ -9,3 +9,19 @@ Author: Muhammmad Reza Heidary
 Author URI: http://URI_Of_The_Plugin_Author
 License: MIT
 */
+
+
+include_once(plugin_dir_path(__FILE__).'libs/notificator-telegram-bot.php');
+
+
+add_action('wp_login_failed', function () {
+    telegram_notificator_send_message('Login Failed');
+});
+
+add_action('wp_head', function () {
+    echo "<!-- This is Header -->".PHP_EOL;
+}, 999);
+
+add_action('wp_footer', function () {
+    echo "<!-- This is Footer -->".PHP_EOL;
+}, 9999);
